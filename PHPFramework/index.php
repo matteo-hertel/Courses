30a9722d821662905df05b43045432f97b506ed8
Matteo Hertel <info@matteohertel.com>
2016-03-06 17:43:56 +0000

    PHPFramework: add Symfony request

--- PHPFramework/index.php
+++ PHPFramework/index.php
@@ -1,6 +1,19 @@
 <?php
 
-echo"<pre style='color:#59E817; background-color:black; word-wrap:break-word;'>";
-var_dump("papoi?");
-echo"</pre>";
- ?>
+require __DIR__ . "/vendor/autoload.php";
+
+use Symfony\Component\HttpFoundation\Request;
+
+$request = Request::createFromGlobals();
+
+
+switch($request->getPathInfo()) {
+        case '/':
+            echo 'This is the home page';
+            break;
+        case '/about':
+            echo 'This is the about page';
+            break;
+        default:
+            echo 'Not found!';
+    }
