db0359edaf7bbceb7975e91372f418d004adf34a
Matteo Hertel <info@matteohertel.com>
2016-03-20 18:16:51 +0000

    Angular2: add base structure and complete lesson 1 and 2

---
+++ JS/Angular2/src/components/02/todo-input.ts
@@ -0,0 +1,7 @@
+import {Component} from "angular2/core";
+
+@Component({
+    selector: "todo-input",
+    template: `<div></div>`
+})
+export class TodoInput { };
---
+++ JS/Angular2/src/main.ts
@@ -0,0 +1,22 @@
+
+
+//These first 3 lines will be deprecated by the final release
+import 'reflect-metadata'
+import 'zone.js'
+import "angular2/angular2";
+
+import {Component} from "angular2/core";
+import {bootstrap} from "angular2/platform/browser";
+import {TodoInput} from "./components/02/todo-input"
+
+@Component({
+    selector:`app`,
+    directives : [TodoInput],
+    template: `<div>
+        <todo-input></todo-input>
+    </div>`
+})
+class App{
+}
+
+bootstrap(App);
