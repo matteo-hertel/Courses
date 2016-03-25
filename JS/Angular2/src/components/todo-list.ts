9397b87bc875859f73c4711395550763b5ae7fc8
Matteo Hertel <info@matteohertel.com>
2016-03-25 23:37:35 +0000

    Angular2: Lesson 13 and 14 pipes complete

--- JS/Angular2/src/components/todo-list.ts
+++ JS/Angular2/src/components/todo-list.ts
@@ -1,13 +1,15 @@
 import {Component} from "angular2/core";
 import {TodoService} from "./../services/todo-service"
 import {TodoItem} from "./todo-item"
+import {SearchPipe} from "./../filters/search"
 
 @Component({
     selector: "todo-list",
+    pipes: [SearchPipe],
     directives: [TodoItem],
     template: `<div>
 <ul>
-<li *ngFor="#todo of todoService.todos">
+<li *ngFor="#todo of todoService.todos | search">
     <todo-item [todo]="todo"></todo-item>
 </li>
 </ul>
