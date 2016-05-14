d4a75855472aec3f81efad17d0b5f9185c685e9a
Matteo Hertel <info@matteohertel.com>
2016-05-14 18:16:59 +0100

    AL: console.assert complete

--- JS/Advanced Logging with the JavaScript Console/assets/js/index.js
+++ JS/Advanced Logging with the JavaScript Console/assets/js/index.js
@@ -1,14 +1,11 @@
 /* jshint node:true */
 /* jshint esnext:true */
 'use strict';
+// standard mode
+var foo;
 
-for (let i = 0; i < 100; i++) {
-    let num = Math.random() * 100;
-    console.groupCollapsed("Picking a random number");
-    console.log("N > 10?", num > 10);
-    console.log("N > 50?", num > 50);
-    console.groupCollapsed("Nested group");
-    console.log(num);
-    console.groupEnd();
-    console.groupEnd();
+if (!foo) {
+    console.error("Foo wasn't set");
 }
+//assert mode
+console.assert(foo, "Foo wans't set");
