f6134853fed6df11da51292802cf19629420768d
Matteo Hertel <info@matteohertel.com>
2016-05-14 18:02:54 +0100

    AL: string substitution complete

--- JS/Advanced Logging with the JavaScript Console/assets/js/index.js
+++ JS/Advanced Logging with the JavaScript Console/assets/js/index.js
@@ -1,5 +1,15 @@
+/**
+ * 5 Semantic Levels
+ */
 console.log("Simple log message");
 console.warn("Warning message!");
 console.error("Error message!");
 console.info("Info message");
 console.debug("Debug message");
+
+//Why is this not available in plain JS??
+console.log("My name is %s ", "Papoi");
+console.log("You can even log object, for instance: %o", {
+    foo: "bar"
+});
+console.log("This is my %coutput", "color:red;font-size:54px;background-color:black);");
