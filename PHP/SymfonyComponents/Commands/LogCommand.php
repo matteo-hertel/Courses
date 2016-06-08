85e6739826d37cfb2a7f9f0287f72201370ef9da
Matteo Hertel <info@matteohertel.com>
2016-06-08 16:25:24 +0100

    Symfony: console lesson complete

--- PHP/SymfonyComponents/Commands/LogCommand.php
+++ PHP/SymfonyComponents/Commands/LogCommand.php
@@ -3,21 +3,25 @@
 namespace Commands;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Output\OutputInterface;
 
 class LogCommand extends Command
 {
-    protected $dateFormat = 'D/m/Y H:i:s';
+    protected $dateFormat = 'd/m/Y H:i:s';
 
     protected function configure()
     {
-        $this->setName('console:log')
-                ->setDescription('Log a message');
+        $this->setName('log')
+                ->setDescription('Log a message')
+                ->addArgument('message', InputArgument::OPTIONAL, 'The message to log', 'default message');
     }
 
-    protected function execute()
+    protected function execute(InputInterface $input, OutputInterface $output)
     {
         $now = new \DateTime();
 
-        echo sprintf('%s: Log message', $no->format($this->dateFormat));
+        $output->writeLn(sprintf('%s: %s', $now->format($this->dateFormat), $input->getArgument('message')));
     }
 }
