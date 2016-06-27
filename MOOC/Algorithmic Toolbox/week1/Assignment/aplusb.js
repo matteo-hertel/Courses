419c352c94bebe56fff9016e9d1c945f8e2fbcb2
Matteo Hertel <info@matteohertel.com>
2016-06-27 14:40:10 +0100

    Remove the JS solution, node is incredibly difficult to use on coursera

--- MOOC/Algorithmic Toolbox/week1/Assignment/aplusb.js
+++
@@ -1,9 +0,0 @@
-/**
- * If arguments are passed through the command line we can use process.argv
- * 0 is the interpreter and 1 is for the script path, everything else is what has been passed through the command line
- *
- */
-const a = parseInt(process.argv[2]) || 0 // first argument or 0
-const b = parseInt(process.argv[3]) || 0 // second argument or 0
-
-console.log(a + b);
