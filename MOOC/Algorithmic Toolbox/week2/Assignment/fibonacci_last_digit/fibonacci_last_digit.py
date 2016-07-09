a9bcf4aaf19a5dbe27a4379dff24eb00bc566f0c
Matteo Hertel <info@matteohertel.com>
2016-07-09 19:48:00 +0100

    MOOC: add fibonacci solutions

--- MOOC/Algorithmic Toolbox/week2/Assignment/fibonacci_last_digit/fibonacci_last_digit.py
+++ MOOC/Algorithmic Toolbox/week2/Assignment/fibonacci_last_digit/fibonacci_last_digit.py
@@ -2,10 +2,13 @@
 import sys
 
 def get_fibonacci_last_digit(n):
-    # write your code here
-    return 0
+    if (n <= 1):
+        return n
+    f= [0,1]
 
-if __name__ == '__main__':
-    input = sys.stdin.read()
-    n = int(input)
-    print(get_fibonacci_last_digit(n))
+    for i in range(2,n + 1):
+        f.append((f[i-1] + f[i-2])%10)
+    return f.pop()
+
+n = int(input())
+print(get_fibonacci_last_digit(n))
