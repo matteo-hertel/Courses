f90c30f926aa34a4947a5b254b99ec557c3b067c
Matteo Hertel <info@matteohertel.com>
2016-07-09 20:26:55 +0100

    MOOC: add gcd

--- MOOC/Algorithmic Toolbox/week2/Assignment/gcd/gcd.py
+++ MOOC/Algorithmic Toolbox/week2/Assignment/gcd/gcd.py
@@ -2,27 +2,15 @@
 import sys
 
 def gcd(a, b):
-    current_gcd = 1
-    for d in range(2, min(a, b) + 1):
-        if a % d == 0 and b % d == 0:
-            if d > current_gcd:
-                current_gcd = d
 
-    return current_gcd
+    if (b == 0):
+        return a;
 
-if __name__ == "__main__":
-    input = sys.stdin.read()
-    a, b = map(int, input.split())
-    print(gcd(a, b))
+    prime = int(a%b);
+    return gcd(b, prime)
+    
 
+input = input()
+a, b = map(int, input.split())
+print(gcd(a, b))
 
-'''
-
-function eu(a, b){
-if(b === 0){
-return a;
-}
-var prime = a/b;
-return eu(b, prime)
-}
-'''
