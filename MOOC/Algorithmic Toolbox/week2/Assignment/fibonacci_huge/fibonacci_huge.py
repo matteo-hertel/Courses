1ac02f85644a2fccf4b9a12c792442f7e22abd56
Matteo Hertel <info@matteohertel.com>
2016-07-10 18:42:43 +0100

    MOOC: add fibonacci huge

--- MOOC/Algorithmic Toolbox/week2/Assignment/fibonacci_huge/fibonacci_huge.py
+++ MOOC/Algorithmic Toolbox/week2/Assignment/fibonacci_huge/fibonacci_huge.py
@@ -1,12 +1,38 @@
 # Uses python3
 import sys
 
+def get_fibonacci_mod(n, m):
+    if (n <= 1):
+        return n
+    f= [0,1]
+
+    for i in range(2,n + 1):
+        f.append((f[i-1] + f[i-2])%m)
+    return f.pop()
+
+def calculate_pisano_period(n, m):
+    if (n <= 1):
+        return n
+    f= [0,1]
+
+    for i in range(2,n + 1):
+        temp = (f[i-1] + f[i-2]) % m
+
+        if( temp == 1 and f[len(f) - 1] == 0):
+            return len(f[:-1])
+        f.append(temp)
+
+
+
 def get_fibonaccihuge(n, m):
-   '''
-    pisano period
-    n mod pisano.lenght = reminder
-    fibonanic mod del reminder
-   '''
+    # pisano period
+    pisano_period = calculate_pisano_period(n, m)
+    # n mod pisano.lenght = reminder
+    reminder = n% pisano_period
+    # fibonanic mod of reminder
+    return get_fibonacci_mod(reminder, m)
+
+
 
 input = input();
 n, m = map(int, input.split())
