f79bae49ca7e4c14eaf14121c902171516b2f1da
Matteo Hertel <info@matteohertel.com>
2016-07-18 00:13:01 +0100

    MOOC: add covering segments solution

--- MOOC/Algorithmic Toolbox/week3/covering_segments/covering_segments.py
+++ MOOC/Algorithmic Toolbox/week3/covering_segments/covering_segments.py
@@ -7,10 +7,26 @@ Segment = namedtuple('Segment', 'start end')
 def optimal_points(segments):
     points = []
     #write your code here
-    for s in segments:
-        points.append(s.start)
-        points.append(s.end)
+
+    start = segments[0][0]
+    end = segments[0][1]
+
+    sortedStart = sorted(segments, key=lambda x: x[0])
+    sortedEnd = sorted(segments, key=lambda x: x[1])
+
+    threshold = sortedStart[0][0] - 1
+    for i in range(len(sortedEnd)-1):
+        start, end = sortedEnd[i]
+        if start > threshold:
+            points.append(end)
+            threshold = end
+
+    if points[len(points) - 1] < sortedEnd[len(sortedEnd) - 1][0]:
+        if sortedEnd:
+            points.append(sortedEnd[len(sortedEnd) - 1][0])
     return points
+
+#segments = [Segment(4,7),Segment(1,3),Segment(2,5),Segment(5,6)]
 
 if __name__ == '__main__':
     input = sys.stdin.read()
