755a51e1c021f7949161e360ef4b2de3e425c597
Matteo Hertel <info@matteohertel.com>
2016-07-31 11:52:56 +0100

    MOOC: add knapsack soultion

--- MOOC/Algorithmic Toolbox/week5/knapsack/knapsack.py
+++ MOOC/Algorithmic Toolbox/week5/knapsack/knapsack.py
@@ -2,12 +2,23 @@
 import sys
 
 def optimal_weight(W, w):
-    # write your code here
-    result = 0
-    for x in w:
-        if result + x <= W:
-            result = result + x
-    return result
+    n = len(w)
+    #inialize all the value (0,j)
+    #inialize all the value (w,0)
+    #
+    optimal_value = [[0 for i in range(n + 1)]  for j in range(W+1)]
+
+    for i in range(1,n + 1): 
+      for weight in range(1,W + 1):
+        optimal_value[weight][i] = optimal_value[weight][i-1]
+        #nth element weigh
+        Wi = w[i-1]
+        if Wi <= weight:
+          value = optimal_value[weight-Wi][i-1] + Wi
+          if value > optimal_value[weight][i]:
+            optimal_value[weight][i] = value
+
+    return optimal_value[W][n]
 
 if __name__ == '__main__':
     input = sys.stdin.read()
