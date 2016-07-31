e390e1c98b3ddd648c144937f632c169bebfd786
Matteo Hertel <info@matteohertel.com>
2016-07-31 11:56:50 +0100

    MOOC: add primitive_calculator solution

--- MOOC/Algorithmic Toolbox/week5/primitive_calculator/primitive_calculator.py
+++ MOOC/Algorithmic Toolbox/week5/primitive_calculator/primitive_calculator.py
@@ -1,20 +1,46 @@
 # Uses python3
 import sys
 
-def optimal_sequence(n):
+mem = {}
+
+def calculate(n):
+    if n == 1:
+        return 1, -1
+    if mem.get(n) is not None:
+        return mem[n]
+    sequence = (calculate(n - 1)[0] + 1, n - 1)
+
+    if n % 2 == 0:
+        ret = calculate(n // 2)
+        if sequence[0] > ret[0]:
+            sequence = (ret[0] + 1, n // 2)
+
+    if n % 3 == 0:
+        ret = calculate(n // 3)
+        if sequence[0] > ret[0]:
+            sequence = (ret[0] + 1, n // 3)
+
+    mem[n] = sequence
+    return sequence
+
+def solution(n):
     sequence = []
-    while n >= 1:
+    while calculate(n)[1] != -1:
         sequence.append(n)
-        if n % 3 == 0:
-            n = n // 3
-        elif n % 2 == 0:
-            n = n // 2
-        else:
-            n = n - 1
-    return reversed(sequence)
+        n = calculate(n)[1]
+    sequence.append(1)
+    sequence.reverse()
+    return sequence;
+
+def optimal_sequence(n):
+    for i in range(1, n):
+        calculate(i)[0]
+    return solution(n)
+
 
 input = sys.stdin.read()
 n = int(input)
+
 sequence = list(optimal_sequence(n))
 print(len(sequence) - 1)
 for x in sequence:
