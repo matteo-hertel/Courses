aaf8471cf3cc4969aa1c6737fd99fe0ea52c260f
Matteo Hertel <info@matteohertel.com>
2016-08-30 22:56:41 +0100

    SymfonyComponents: Add working event dispatcher

--- PHP/SymfonyComponents/EventDispatcher/index.php
+++ PHP/SymfonyComponents/EventDispatcher/index.php
@@ -1,3 +1,35 @@
 <?php
-
+/**
+ *  Up to 07:04
+ */
 require __DIR__ . "/vendor/autoload.php";
+
+use Symfony\Component\EventDispatcher\Event;
+use Symfony\Component\EventDispatcher\EventDispatcher;
+
+$dispatcher = new EventDispatcher();
+
+$dispatcher->addListener("UserSignedUp", function(Event $event){
+    echo"<pre style='color:#59E817; background-color:black; word-wrap:break-word;'>";
+    var_dump("send welcome email", $event->user);
+    echo"</pre>";
+});
+$dispatcher->addListener("UserSignedUp", function(Event $event){
+    echo"<pre style='color:#59E817; background-color:black; word-wrap:break-word;'>";
+    var_dump("send followup email", $event->user);
+    echo"</pre>";
+});
+/**
+ * ``UserSignedUp
+ */
+class UserSignedUp extends Event
+{
+public $user;
+
+    function __construct($user)
+    {
+        $this->user = $user;
+    }
+}
+$event = new UserSignedUp((object)["name" => "Jonnho"]);
+$dispatcher->dispatch("UserSignedUp", $event);
