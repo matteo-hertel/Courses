80dc136777cc1595b8262e7bb406b4b78db17874
Matteo Hertel <info@matteohertel.com>
2016-09-01 03:00:40 +0100

    SymfonyComponents: add full namepsace for naming events

--- PHP/SymfonyComponents/EventDispatcher/index.php
+++ PHP/SymfonyComponents/EventDispatcher/index.php
@@ -1,24 +1,27 @@
 <?php
+
+/**
+ * https://laracasts.com/series/discover-symfony-components/episodes/4
+ * up to 10:59
+ */
 require __DIR__ . "/vendor/autoload.php";
 
 use Symfony\Component\EventDispatcher\Event;
 use Symfony\Component\EventDispatcher\EventDispatcher;
-use App\Events;
-echo"<pre style='color:#59E817; background-color:black; word-wrap:break-word;'>";
-var_dump(class_exists("App\Events\UserSignedUp"));
-echo"</pre>";
+use App\Events\UserSignedUp;
+
 $dispatcher = new EventDispatcher();
 
-$dispatcher->addListener("UserSignedUp", function(Event $event){
+$dispatcher->addListener(UserSignedUp::class, function(Event $event){
     echo"<pre style='color:#59E817; background-color:black; word-wrap:break-word;'>";
     var_dump("send welcome email", $event->user);
     echo"</pre>";
 });
-$dispatcher->addListener("UserSignedUp", function(Event $event){
+$dispatcher->addListener(UserSignedUp::class, function(Event $event){
     echo"<pre style='color:#59E817; background-color:black; word-wrap:break-word;'>";
     var_dump("send followup email", $event->user);
     echo"</pre>";
 });
 
 $event = new UserSignedUp((object)["name" => "Jonnho"]);
-$dispatcher->dispatch("UserSignedUp", $event);
+$dispatcher->dispatch(UserSignedUp::class, $event);
