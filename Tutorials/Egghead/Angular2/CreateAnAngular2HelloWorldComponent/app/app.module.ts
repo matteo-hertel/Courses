3c9d3a77bf795997f46e477eaebf4a8d159d5990
Matteo Hertel <info@matteohertel.com>
2016-09-19 16:39:30 +0200

    [EggHead] Angular2 add home component

--- Tutorials/Egghead/Angular2/CreateAnAngular2HelloWorldComponent/app/app.module.ts
+++ Tutorials/Egghead/Angular2/CreateAnAngular2HelloWorldComponent/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HomeModule } from "./home/home.module"
 import { AppComponent } from './app.component';
 
 @NgModule({
-    imports: [BrowserModule],
-    declarations: [AppComponent],
+    imports: [BrowserModule, HomeModule],
+    declarations: [AppComponent, ],
     bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ Tutorials/Egghead/Angular2/CreateAnAngular2HelloWorldComponent/app/home/home.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "home",
+    template: `
+<div>I'm Home component</div>
+`
+})
+export class HomeComponent{}
