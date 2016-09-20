b37793867598747060cf5092ed62d606920ed6a4
Matteo Hertel <info@matteohertel.com>
2016-09-20 15:06:44 +0200

    [EggHead] finish Style Angular 2 Components

--- Tutorials/Egghead/Angular2/CreateAnAngular2HelloWorldComponent/app/widgets/widget-one.component.ts
+++ Tutorials/Egghead/Angular2/CreateAnAngular2HelloWorldComponent/app/widgets/widget-one.component.ts
@@ -2,6 +2,14 @@ import { Component, Input } from "@angular/core";
 
 @Component({
     selector: "widget-one",
+    styles: [`
+:host{
+    display: flex;
+    border: 3px dashed black;
+    font-family: "Times New Roman";
+}
+`
+    ],
     template: `
 <h2>One's message:</h2>
 <h3>{{message}}</h3>
