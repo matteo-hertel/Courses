133eb692c571ebf21c21b716f819b9a82cb91de8
Matteo Hertel <info@matteohertel.com>
2016-09-20 15:44:53 +0200

    [EggHead] finish Manipulate and Access DOM Elements with Angular 2 Renderer

--- Tutorials/Egghead/Angular2/CreateAnAngular2HelloWorldComponent/app/widgets/widgets.module.ts
+++ Tutorials/Egghead/Angular2/CreateAnAngular2HelloWorldComponent/app/widgets/widgets.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { WidgetOne } from "./widget-one.component"
 import { WidgetTwo } from "./widget-two.component"
+import { WidgetThree } from "./widget-three.component"
 import { CommonModule } from "@angular/common"
 @NgModule({
     imports: [CommonModule],
-    declarations: [WidgetOne, WidgetTwo],
-    exports: [WidgetOne, WidgetTwo, CommonModule]
+    declarations: [WidgetOne, WidgetTwo, WidgetThree],
+    exports: [WidgetOne, WidgetTwo, WidgetThree, CommonModule]
 })
 
 export class WidgetsModule { }
