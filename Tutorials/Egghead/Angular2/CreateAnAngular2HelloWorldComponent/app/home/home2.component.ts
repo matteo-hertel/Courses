e8979d9bf8fb20043e64e4884786bfbddf05a7b6
Matteo Hertel <info@matteohertel.com>
2016-09-20 16:25:39 +0200

    [EggHead] finish Order Dynamic Components Inside an Angular 2 ViewContainer

--- Tutorials/Egghead/Angular2/CreateAnAngular2HelloWorldComponent/app/home/home2.component.ts
+++ Tutorials/Egghead/Angular2/CreateAnAngular2HelloWorldComponent/app/home/home2.component.ts
@@ -1,19 +1,21 @@
-import { Component, ViewChild, ViewContainerRef,ComponentFactoryResolver } from "@angular/core";
+import { Component, ViewChild, ViewContainerRef, ComponentFactoryResolver } from "@angular/core";
 import { SimpleService } from "./../services/services.module"
 import { WidgetThree } from "./../widgets/widget-three.component";
 @Component({
     selector: "home2",
     template: `
+<button (click)="addComponent()">Add Component</button>
 <div #container></div>
 `
 })
 export class HomeComponent2 {
-    @ViewChild("container", {read:ViewContainerRef}) container: ViewContainerRef;
+    addedComponent:number = 0;
+    @ViewChild("container", { read: ViewContainerRef }) container: ViewContainerRef;
 
-    constructor(private resolver:ComponentFactoryResolver){}
+    constructor(private resolver: ComponentFactoryResolver) { }
 
     ngAfterContentInit() {
-const widgetFactory = this.resolver.resolveComponentFactory(WidgetThree);
+        const widgetFactory = this.resolver.resolveComponentFactory(WidgetThree);
         this.container.createComponent(widgetFactory);
         this.container.createComponent(widgetFactory);
         this.container.createComponent(widgetFactory);
@@ -24,7 +26,14 @@ const widgetFactory = this.resolver.resolveComponentFactory(WidgetThree);
         this.container.createComponent(widgetFactory);
         this.container.createComponent(widgetFactory);
         this.container.createComponent(widgetFactory);
+        const widgetRef = this.container.createComponent(widgetFactory, 2);
+        widgetRef.instance.message = "I'm third!";
+    }
+
+    addComponent() {
+this.addedComponent++;
+        const widgetFactory = this.resolver.resolveComponentFactory(WidgetThree);
         const widgetRef = this.container.createComponent(widgetFactory);
-        widgetRef.instance.message = "Papoi there!";
+        widgetRef.instance.message = `I'm added component n ${this.addedComponent}!`;
     }
 }
