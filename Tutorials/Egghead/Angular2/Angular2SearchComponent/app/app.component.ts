142722ed1a92128df2847bdd9bc915684b9b7e2a
Matteo Hertel <info@matteohertel.com>
2016-09-28 12:59:55 +0200

    [EggHead] finish Group Inputs in Angular 2 Forms with ngModelGroup

--- Tutorials/Egghead/Angular2/Angular2SearchComponent/app/app.component.ts
+++ Tutorials/Egghead/Angular2/Angular2SearchComponent/app/app.component.ts
@@ -4,20 +4,23 @@ import { Component } from '@angular/core';
     selector: 'my-app',
     template: `
 <form #formRef="ngForm" (ngSubmit)="onSubmit(formRef.value)">
-<input
-name="username"
-type="text"
-[(ngModel)]="username"
-#usernameRef="ngModel"
-required
-minlength="3"
->
-<div *ngIf="usernameRef.errors?.required">This field is required</div>
-<div *ngIf="usernameRef.errors?.minlength">
-This field must be longer than {{usernameRef.errors?.minlength.requiredLength}} characters.
-You only typed <b>{{usernameRef.errors?.minlength.actualLength}}</b> carachter(s).
-</div>
-<button type="submit">Submit</button>
+    <fieldset ngModelGroup="login">
+        <input
+            name="username"
+            type="text"
+            [(ngModel)]="username"
+            #usernameRef="ngModel"
+            required
+            minlength="3"
+        >
+        <div *ngIf="usernameRef.errors?.required">This field is required</div>
+        <div *ngIf="usernameRef.errors?.minlength">
+            This field must be longer than {{usernameRef.errors?.minlength.requiredLength}} characters.
+            You only typed <b>{{usernameRef.errors?.minlength.actualLength}}</b> carachter(s).
+        </div>
+        <input type="password" ngModel name="password">
+    </fieldset>
+    <button type="submit">Submit</button>
 </form>
 {{formRef.value | json}}
 {{formRef.valid | json}}
