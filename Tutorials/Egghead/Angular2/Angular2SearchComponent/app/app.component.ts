af8fe15fce7e842d05527abea9c70b8ffb8c7c27
Matteo Hertel <info@matteohertel.com>
2016-09-28 12:55:57 +0200

    [EggHead] finish Create and Submit an Angular 2 Form using ngForm

--- Tutorials/Egghead/Angular2/Angular2SearchComponent/app/app.component.ts
+++ Tutorials/Egghead/Angular2/Angular2SearchComponent/app/app.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'my-app',
     template: `
+<form #formRef="ngForm" (ngSubmit)="onSubmit(formRef.value)">
 <input
+name="username"
 type="text"
 [(ngModel)]="username"
 #usernameRef="ngModel"
@@ -15,8 +17,15 @@ minlength="3"
 This field must be longer than {{usernameRef.errors?.minlength.requiredLength}} characters.
 You only typed <b>{{usernameRef.errors?.minlength.actualLength}}</b> carachter(s).
 </div>
+<button type="submit">Submit</button>
+</form>
+{{formRef.value | json}}
+{{formRef.valid | json}}
 `
 })
 export class AppComponent {
-    private username: string = "lol";
+    private username: string = "Papoi";
+    onSubmit(formValue:any) {
+        console.log(formValue);
+    }
 }
