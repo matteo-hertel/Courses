ae0edff73ed11bc1ef9c1a650d44d35cee0a76a1
Matteo Hertel <info@matteohertel.com>
2016-09-28 12:48:49 +0200

    [EggHead] finish Display Validation and Error Messaging in Angular 2

--- Tutorials/Egghead/Angular2/Angular2SearchComponent/app/app.component.ts
+++ Tutorials/Egghead/Angular2/Angular2SearchComponent/app/app.component.ts
@@ -3,19 +3,20 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'my-app',
     template: `
-<input required type="text"
+<input
+type="text"
+[(ngModel)]="username"
 #usernameRef="ngModel"
-[(ngModel)]="username">
-<p>Valid: {{usernameRef.valid}}</p>
-<pre>
-{{this | json}}
-</pre>
-
+required
+minlength="3"
+>
+<div *ngIf="usernameRef.errors?.required">This field is required</div>
+<div *ngIf="usernameRef.errors?.minlength">
+This field must be longer than {{usernameRef.errors?.minlength.requiredLength}} characters.
+You only typed <b>{{usernameRef.errors?.minlength.actualLength}}</b> carachter(s).
+</div>
 `
 })
 export class AppComponent {
-    private username: string = "Papoi";
-    test(): string {
-        return "test";
-    }
+    private username: string = "lol";
 }
