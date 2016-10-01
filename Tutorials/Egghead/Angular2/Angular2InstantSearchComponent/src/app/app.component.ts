64291fcc105817a4ef1720f269cbe9ee7ef51d92
Matteo Hertel <info@matteohertel.com>
2016-10-01 17:43:58 +0200

    [EggHead] Consuming events as Observables in Angular 2 done

--- Tutorials/Egghead/Angular2/Angular2InstantSearchComponent/src/app/app.component.ts
+++ Tutorials/Egghead/Angular2/Angular2InstantSearchComponent/src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { WikipediaSearchService } from './wikipedia-search.service';
+import { Subject } from "rxjs/Subject";
+import "rxjs/add/operator/map";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    // moduleId: module.id,
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    providers: [WikipediaSearchService]
 })
 export class AppComponent {
-  title = 'app works!';
+    items: string[];
+    term$ = new Subject<string>();
+    constructor(private service: WikipediaSearchService) { }
+
+    search(term: string) {
+        this.service.search(term).subscribe(results => this.items = results);
+    }
+
+    ngOnInit() {
+        this.term$.subscribe(term => this.search(term));
+    }
 }
