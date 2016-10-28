46413cb3db0e429a25982c3d4e164ad5d11358d1
Matteo Hertel <info@matteohertel.com>
2016-10-28 16:25:50 +0400

    [EggHead] Add code base

---
+++ Tutorials/Egghead/JS/Misc/TestingES6Promises/index.js
@@ -0,0 +1,18 @@
+/* jshint esversion : 6 */
+/* jshint node : true */
+exports.foo = (opts) => {
+    return new Promise((resolve, reject) => {
+        if (opts === 1) {
+            reject("Found an error");
+        } else {
+            setTimeout(() => {
+                console.log(opts);
+                resolve(opts);
+            }, 500);
+        }
+    });
+};
+
+exports.foo(2).catch(err => {
+    console.log(err);
+});
